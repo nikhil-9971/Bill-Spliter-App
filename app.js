6e4a02bbb2adb6d56cf68498c6af23c4d50cad7c
nikhil-9971 <124913952+nikhil-9971@users.noreply.github.com>
2023-05-06 13:13:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ billDivideForm.addEventListener("submit", (event) => {
 
 function getInputs() {
   let subtotal = document.querySelector("#subtotal").value;
+  let discount = document.querySelector("#discount").value;
   let tipPercent = document.querySelector("#tip").value;
   let noOfPerson = document.querySelector("#no-of-person").value;
 
-  return { subtotal, tipPercent, noOfPerson };
+  return { subtotal, tipPercent, noOfPerson, discount };
 }
 
 // Calculate the bill divider info,
@@ -20,22 +21,36 @@ function getInputs() {
 function showCalculateOutputs(input) {
   billDivideForm.reset();
 
-  let totaltip =
-    (parseFloat(input.subtotal) * parseFloat(input.tipPercent)) / 100;
+  
+  let totalDiscount =
+    (parseFloat(input.subtotal) * parseFloat(input.discount)) / 100;
 
-  let totalAmount = parseFloat(input.subtotal) + totaltip;
+  let totaltip =
+    // (((parseFloat(input.subtotal) * parseFloat(input.tipPercent))-totalDiscount) / 100 ) ;
+    ((parseFloat(input.subtotal)-totalDiscount) * parseFloat(input.tipPercent)) /100;
+
+
+  let totalAmount = parseFloat(input.subtotal) + totaltip - totalDiscount;
 
   let tipPerPerson = totaltip / parseFloat(input.noOfPerson);
 
-  let billPerPerson = totalAmount / parseFloat(input.noOfPerson);
+  let billPerPerson =
+    (totalAmount) / parseFloat(input.noOfPerson);
 
   document.getElementById(
     "total-bill"
   ).innerHTML = `${rupee} ${totalAmount.toFixed(2)}`;
 
-  document.getElementById("bill-per-person").innerHTML = `${rupee} ${billPerPerson.toFixed(2)}`;
-
-  document.getElementById("total-tip").innerHTML = `${rupee} ${totaltip.toFixed(2)}`;
-  document.getElementById("tip-per-person").innerHTML = `${rupee} ${tipPerPerson.toFixed(2)}`;
+  document.getElementById(
+    "bill-per-person"
+  ).innerHTML = `${rupee} ${billPerPerson.toFixed(2)}`;
+  document.getElementById(
+    "total-discount"
+  ).innerHTML = `${rupee} ${totalDiscount.toFixed(2)}`;
+  document.getElementById("total-tip").innerHTML = `${rupee} ${totaltip.toFixed(
+    2
+  )}`;
+  document.getElementById(
+    "tip-per-person"
+  ).innerHTML = `${rupee} ${tipPerPerson.toFixed(2)}`;
 }
-
